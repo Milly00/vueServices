98b7bf7ed344378668459b835b2881602292b7b0
Milly <benitezmilly3@gmail.com>
2025-04-13 16:46:59 -0500

    Creación de servicios

---
+++ src/components/PostList.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import PostService from '@/services/PostService';
+import { onMounted } from 'vue';
+const service = new PostService;
+const posts = service.getPosts();
+onMounted(async ()=>{
+   await service.fetchAll()
+});
+
+
+</script>
+<template>
+   <div v-for="post in posts" :key="post.id">
+   <h3>{{ post.title }}</h3>
+   </div>
+</template>
+<style scoped>
+    .post-list{
+        width: 95vw;
+        height: 75px;
+        padding: 20px;
+    }
+</style>
